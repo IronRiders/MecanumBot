b67d5b1f18a5b89f3aa7f37df97846fd2693a3ba
Alex Fischman <alex@fischman.org>
2019-09-30 22:10:47 -0700

    Made axes and buttons private and added getters

--- src/main/java/frc/robot/Controller.java
+++ src/main/java/frc/robot/Controller.java
@@ -5,8 +5,8 @@ import edu.wpi.first.wpilibj.GenericHID;
 public class Controller extends GenericHID {
     private static double kDeadzone = 0.1;
 
-    public double[] axes;
-    public boolean[] buttons;
+    private double[] axes;
+    private boolean[] buttons;
 
     public Controller(final int port) {
         super(port);
@@ -37,15 +37,21 @@ public class Controller extends GenericHID {
         }
     }
 
+    public double[] getAxes() {
+        return this.axes;
+    }
+
+    public boolean[] getButtons() {
+        return this.buttons;
+    }
+
     @Override
     public double getX(Hand hand) {
-        this.update();
         return this.axes[0];
     }
 
     @Override
     public double getY(Hand hand) {
-        this.update();
         return this.axes[1];
     }
 }
