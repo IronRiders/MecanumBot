e826c9ea305314b22e4c254b10230d3049278327
Alex Fischman <alex@fischman.org>
2019-09-30 22:57:46 -0700

    Moved logic from Robot into Main

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,10 +1,29 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.RobotBase;
-import frc.robot.MecanumRobot;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import frc.robot.Controller;
+import frc.robot.MecanumDrive;
 
 public class Main {
     public static final void main(String[] args) {
-        RobotBase.startRobot(MecanumRobot::new);
+        Controller controller = new Controller(0);
+        MecanumDrive mecanumDrive = new MecanumDrive();
+
+        Runnable update = new Runnable() {
+            public void run() {
+                controller.update();
+        
+                final double x = controller.getAxes()[0];
+                final double y = controller.getAxes()[1];
+                final double w = controller.getAxes()[3];
+                
+                mecanumDrive.updateSpeed(x, y, w);
+            }
+        };
+        
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(update, 0, 20, TimeUnit.MILLISECONDS);
     }
 }
