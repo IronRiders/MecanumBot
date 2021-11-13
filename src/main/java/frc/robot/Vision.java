3b5a7dbf0a853fe0561f85e9a8cabf2843127971
Jacob6819 <45805625+Jacob6819@users.noreply.github.com>
2021-11-13 11:48:27 -0800

    Added matrix calculations

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -7,10 +7,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Vision {
     private NetworkTable table;
-    private NetworkTableEntry hasTarget;
-    private NetworkTableEntry tx;
-    private NetworkTableEntry ty;
-    private NetworkTableEntry ta;
+    private NetworkTableEntry hasTarget, tx, ty, ta;
     private final double TURN_SPEED = .1;
     private final double FORWARD_SPEED = .2;
     private final double MAX_SPEED = .2;
@@ -32,28 +29,17 @@ public class Vision {
         System.out.println("tx: " + getTx() + "\tty: " + getTy() + "\tDistance " + distanceToTarget());
     }
 
-    public double[] driveToTarget() {
-        double driveInstructions[] = { 0.0, 0.0, 0.0 };
-        if (getHasTarget()) {
-            driveInstructions[1] = distanceToTarget() * FORWARD_SPEED;
-            driveInstructions[2] = getTx() * TURN_SPEED;
-            for (int i = 0; i < driveInstructions.length; i++) {
-                if (Math.abs(driveInstructions[i]) < MIN_SPEED) {
-                    driveInstructions[i] = 0;
-                } else if (Math.abs(driveInstructions[i]) > MAX_SPEED) {
-                    driveInstructions[i] = MAX_SPEED * isNegative(driveInstructions[i]);
-                }
-            }
-        }
-
-        return driveInstructions;
-
-    }
+    
 
     public double distanceToTarget() {
         return (TARGET_HIGHT - CAMERA_HIGHT) / (Math.tan(toRadians(getTy() + CAMERA_ANGLE)));
     }
 
+    //This method is because there are diffrent target hights that we might want to use.
+    private double determineObjectDist(double cameraHeight, double targetHight){
+        return (targetHight - cameraHeight) / (Math.tan(toRadians(CAMERA_ANGLE + getTy())));
+    }
+    
     public double toRadians(double x) {
         return Math.PI * x / 180.0;
     }
@@ -82,4 +68,86 @@ public class Vision {
         }
     }
 
+ // Returns the reduced row-echelon form of a matrix.
+private double[][] Gaussian(double[][] matrix) {
+    int n = matrix.length;
+    int m = matrix[0].length;
+    double[][] echelon = new double[n][];
+    for(int i = 0; i < matrix.length; i++) {
+      echelon[i] = matrix[i].clone();
+    }
+
+    while (!isEchelon(echelon)) {
+      for (int i = 0; i < Math.min(n, m); i++) {
+        double pivot = matrix[i][i];
+        if (pivot != 0) {
+          if (pivot != 1) {
+            for (int j = 0; j < m; j++) { 
+                echelon[i][j] /= pivot; 
+            }
+          }
+          for (int rowNum = 0; rowNum < n; rowNum++) {
+            for (int j = 0; j < m; j++) { 
+                echelon[rowNum][j] -= echelon[rowNum][i] * echelon[i][j];
+            }
+          }
+        }
+      }
+    }
+    return echelon;
+  }
+
+  // Tests whether a matrix is in reduced-row-echelon form
+  private boolean isEchelon(double[][] matrix) {
+    int n = matrix.length;
+    int m = matrix[0].length;
+    double[][] matrixTranspose = transpose(matrix);
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        double val = matrix[i][j];
+        if (Math.abs(val) > 0) {
+          int non_zero_count = 0;
+          for (int k = 0; k < n; k++) {
+            if (matrixTranspose[j][k] != 0) { non_zero_count += 1; }
+          }
+          if (non_zero_count != 1){ return false; }
+        }
+      }
+    }
+    return true;
+  }
+
+    // Returns the transpose of a matrix.
+    private double[][] transpose(double[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        double[][] matrixTranspose = new double[m][n];
+        for (int i = 0; i < n; i++) {
+          for (int j = 0; j < m; j++) {
+            matrixTranspose[m][n] = matrix[n][m];
+          }
+        }
+        return matrixTranspose;
+      }
+
+
+
+
+    // public double[] driveToTarget() {
+    //         double driveInstructions[] = { 0.0, 0.0, 0.0 };
+    //         if (getHasTarget()) {
+    //             driveInstructions[1] = distanceToTarget() * FORWARD_SPEED;
+    //             driveInstructions[2] = getTx() * TURN_SPEED;
+    //             for (int i = 0; i < driveInstructions.length; i++) {
+    //                 if (Math.abs(driveInstructions[i]) < MIN_SPEED) {
+    //                     driveInstructions[i] = 0;
+    //                 } else if (Math.abs(driveInstructions[i]) > MAX_SPEED) {
+    //                     driveInstructions[i] = MAX_SPEED * isNegative(driveInstructions[i]);
+    //                 }
+    //             }
+    //         }
+
+    //         return driveInstructions;
+
+    //     }
 }
